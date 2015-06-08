63a8b2911058f32ef67d4dbb2dd7f2e4a2eee26d
abhinay302 <abhinay302@gmail.com>
2015-06-09 01:25:19 +0530

    Add Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  root 'gratitudes#index'
   resources :gratitudes
 
   # The priority is based upon order of creation: first created -> highest priority.
